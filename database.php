971d1478ede5f45f8b3d15e4069e35417df87f7e
Riopacteres <pacteres.rio@ici.edu.ph>
2025-09-21 04:56:16 +0800

    Add files via upload

--- database.php
+++ database.php
@@ -13,5 +13,42 @@ if (!$conn) {
     echo "Connected successfully <br>";
 }
 
+echo "
+<style>
+ 
+    body {
+        font-family: Arial, sans-serif;
+        margin: 20px;
+        text-align: center;
+    }
+    h1 {
+        color: #333;
+    }
+    table {
+        border-collapse: collapse;
+        width: 80%;
+        margin-bottom: 30px;
+        box-shadow: 0 2px 8px rgba(14, 3, 3, 0.1);
+        margin-left: auto;
+        margin-right: auto;
+    }
+    th, td {
+        border: 1px solid #ddd;
+        padding: 10px;
+        text-align: center;
+    }
+    th {
+        background-color: blue;
+        color: white;
+    }
+    tr:nth-child(even) {
+        background-color: #f9f9f9;
+    }
+    tr:hover {
+        background-color: #f1f1f1;
+    }
+       
+</style>
+";
 
 ?>
