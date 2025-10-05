1bab646ef2e4285de47ee685befbaecbd474531e
Riopacteres <pacteres.rio@ici.edu.ph>
2025-10-05 16:55:10 +0800

    Add files via upload

---
+++ car_air_condi.php/database.php
@@ -0,0 +1,51 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database_name = "car_air_condi_db";
+
+$conn = mysqli_connect($servername, $username, $password, $database_name);
+
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error($conn));
+} 
+echo "
+<style>
+ 
+    body {
+        font-family: Arial, sans-serif;
+        margin: 20px;
+        text-align: center;
+    }
+    h1 {
+        color: #333;
+    }
+    table {
+        border-collapse: collapse;
+        width: 80%;
+        margin-bottom: 30px;
+        box-shadow: 0 2px 8px rgba(14, 3, 3, 0.1);
+        margin-left: auto;
+        margin-right: auto;
+    }
+    th, td {
+        border: 1px solid #ddd;
+        padding: 10px;
+        text-align: center;
+    }
+    th {
+        background-color: blue;
+        color: white;
+    }
+    tr:nth-child(even) {
+        background-color: #f9f9f9;
+    }
+    tr:hover {
+        background-color: #f1f1f1;
+    }
+       
+</style>
+";
+
+?>
---
+++ car_air_condi.php/inventorypart.php
@@ -0,0 +1,26 @@
+<?php
+include('database.php'); 
+
+$sql = "SELECT * FROM inventorypart";
+$query = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($query) > 0) {
+    echo "<h1>inventorypart table </h1>";
+    echo "<table border='1'>";
+    echo "<tr><th>Part ID</th><th>PartName</th><th>Description</th><th>QuantityInStock</th><th>SupplierName</th></tr>";
+
+    while ($row = mysqli_fetch_assoc($query)) {
+        echo "<tr>";
+        echo "<td>".$row["Part_ID"]."</td>";
+        echo "<td>".$row["PartName"]."</td>";
+        echo "<td>".$row["Description"]."</td>";
+        echo "<td>".$row["QuantityInStock"]."</td>";
+        echo "<td>".$row["SupplierName"]."</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+} else {
+    echo "No results found.<br>";
+}
+?>
